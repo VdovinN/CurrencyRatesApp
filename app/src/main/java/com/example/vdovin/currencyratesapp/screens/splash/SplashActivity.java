c0581b2bde93a15b6e533bcae06758fdea41da7e
VdovinN <VdovinN@users.noreply.github.com>
2017-01-05 17:51:54 +0200

    Added splash screen with logo icon (#2)

---
+++ app/src/main/java/com/example/vdovin/currencyratesapp/screens/splash/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.vdovin.currencyratesapp.screens.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.vdovin.currencyratesapp.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int DELAY_MILLIS = 2000;
+    private Handler handler = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handler = new Handler();
+        handler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, DELAY_MILLIS
+        );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+}
