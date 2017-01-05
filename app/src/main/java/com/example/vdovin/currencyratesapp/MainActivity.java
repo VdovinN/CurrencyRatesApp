7751e8931811796553f8911db9c7a38eaa0fa980
vdovin <ennvdovin@gmail.com>
2017-01-05 12:50:58 +0200

    add develop branch

--- app/src/main/java/com/example/vdovin/currencyratesapp/MainActivity.java
+++ app/src/main/java/com/example/vdovin/currencyratesapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.vdovin.currencyratesapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //develop
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
